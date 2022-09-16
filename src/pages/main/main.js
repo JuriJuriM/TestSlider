9fae1570ede4879263c1f2ebcb0166ee92346e35
JuriJuriM <jurimudry@gmail.com>
2022-09-16 17:30:46 +0300

    bag styles

--- src/pages/main/main.js
+++ src/pages/main/main.js
@@ -1,1 +1,7 @@
 import './main.scss';
+
+let sliderSlider = document.querySelector('.slider__slider');
+let sliderNum = document.querySelector('.slider__num');
+sliderSlider.oninput = function () {
+    sliderNum.innerHTML = this.value;
+}
