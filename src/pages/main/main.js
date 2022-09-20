fd36226463a38ebb764e622ddefb6fbf62a20719
JuriJuriM <jurimudry@gmail.com>
2022-09-20 15:36:17 +0300

    first information

--- src/pages/main/main.js
+++ src/pages/main/main.js
@@ -1,9 +1,1 @@
 import './main.scss';
-
-const sliderSlider = document.querySelector('.slider__slider');
-const sliderNum = document.querySelector('.slider__num');
-
-const callBack = (e) => {
-  sliderNum.innerHTML = e.target.value;
-};
-sliderSlider.addEventListener('input', callBack);
