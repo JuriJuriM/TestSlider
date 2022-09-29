be7e8c1aad8cfacf5fd268cf0dfdf5a6e2b59045
JuriJuriM <jurimudry@gmail.com>
2022-09-29 15:47:32 +0300

    changes

--- src/components/Observer/observer.js
+++ src/components/Observer/observer.js
@@ -1,5 +1,5 @@
 // class Observer
-class Subject {
+class Observer {
   constructor() {
     this.observers = [];
   }
@@ -12,36 +12,31 @@ class Subject {
     this.observers = this.observers.filter(obs => obs !== observer);
   }
 
-  fire(action) {
+  update(action) {
     this.observers.forEach(observer => {
       observer.update(action);
     });
   }
 }
-class Observer {
-  constructor(state) {
-    this.state = state;
-    this.initialState = state;
+
+update(action) {
+  switch (action.type) {
+    case 'ADDELEMENT':
+      this.state = ++this.state;
+      break;
+
+    case 'DELLELEMENT':
+      this.state = --this.state;
+      break;
+
+    case 'CHANGES':
+      this.state += action.payload;
+      break;
+
+    default:
+      this.state = this.initialState;
   }
-
-  update(action) {
-    switch (action.type) {
-      case 'ADDELEMENT':
-        this.state = ++this.state;
-        break;
-
-      case 'DELLELEMENT':
-        this.state = --this.state;
-        break;
-
-      case 'CHANGES':
-        this.state += action.payload;
-        break;
-
-      default:
-        this.state = this.initialState;
-    }
-  }
+}
 }
 
 const stream$ = new Subject()
