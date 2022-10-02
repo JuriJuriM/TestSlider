c4893ec20a43149c0be65e249111b8221ef0c0cf
JuriJuriM <jurimudry@gmail.com>
2022-10-02 18:50:29 +0300

    cleaning

--- src/components/Observer/observer.js
+++ src/components/Observer/observer.js
@@ -18,30 +18,3 @@ class Observer {
     });
   }
 }
-
-// update(action) {
-//   switch (action.type) {
-//     case 'ADDELEMENT':
-//       this.state = ++this.state;
-//       break;
-
-//     case 'DELLELEMENT':
-//       this.state = --this.state;
-//       break;
-
-//     case 'CHANGES':
-//       this.state += action.payload;
-//       break;
-
-//     default:
-//       this.state = this.initialState;
-//   }
-// }
-
-
-// const stream$ = new Subject()
-
-// stream$.fire({ type: 'ADDELEMENT' });
-// stream$.fire({ type: 'DELLELEMENT' });
-// stream$.fire({ type: 'CHANGES', payload: 10 });
-
