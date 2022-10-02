8d8e724866e961234d3aeacd70e91dc4dea0418f
JuriJuriM <jurimudry@gmail.com>
2022-10-02 14:27:20 +0300

    good update

--- src/components/View/SubView/Track.js
+++ src/components/View/SubView/Track.js
@@ -1,27 +1,26 @@
 class Track {
   constructor(slider) {
-    super(slider);
+    this.slider = slider;
     this.init();
+  }
+
+  createSubView() {
+    this.subView = document.createElement('div');
+    this.subView.classList.add('slider__track');
+    this.progress = document.createElement('div');
+    this.progress.classList.add('slider__progress');
+    this.subView.appendChild(this.progress);
+    this.slider.appendChild(this.subView);
+  }
+
+  init() {
+    this.createSubView();
+  }
+
+  update(data) {
+    const { from } = data;
+    this.progress.style.width = `${from}%`;
   }
 }
 
-createSubView() {
-  this.subView = document.createElement('div');
-  this.subView.classList.add('slider__track');
-  this.progress = document.createElement('div');
-  this.subView.classList.add('slider__progress');
-  this.subView.appendChild(this.progress);
-  this.slider.appendChild(this.subView);
-}
-
-protected init() {
-  this.createSubView();
-  this.registerEvent('updateSubView');
-  this.bindEventListener();
-}
-
-protected update() {
-  const {
-    min, max, from, to, horisontal, range, progress
-  } = this.state;
-}
+export default Track;
--- src/pages/main/main.js
+++ src/pages/main/main.js
@@ -1,1 +1,6 @@
 import './main.scss';
+import Track from '../../components/View/SubView/Track';
+
+const slider = document.querySelector('.slider');
+const track = new Track(slider);
+track.update({ from: 50 });
