865c6b18c69fd960f43081b50eaaaa029063bf62
JuriJuriM <jurimudry@gmail.com>
2022-10-03 15:55:28 +0300

    live some code

--- src/components/View/SubView/Handle.js
+++ src/components/View/SubView/Handle.js
@@ -12,9 +12,9 @@ class Handle {
   createSubView() {
     this.subView = document.createElement('div');
     this.subView.classList.add('slider__handle');
-    this.progress = document.createElement('div');
-    this.progress.classList.add('slider__progress');
-    this.subView.appendChild(this.progress);
+    // this.progress = document.createElement('div');
+    // this.progress.classList.add('slider__progress');
+    // this.subView.appendChild(this.progress);
     this.slider.appendChild(this.subView);
   }
 
