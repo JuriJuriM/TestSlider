da2d630ff282f41bfe25177d5edc2172096f168e
JuriJuriM <jurimudry@gmail.com>
2022-10-06 16:26:38 +0300

    start view.js

---
+++ src/components/View/View.js
@@ -0,0 +1,19 @@
+class View {
+  constructor(slider) {
+    this.slider = slider;
+    this.components = [];
+    this.init();
+  }
+
+  init() {
+
+  }
+
+  createSlider() {
+
+  }
+
+  update() {
+
+  }
+}
